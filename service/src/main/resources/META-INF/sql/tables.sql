7693a21742a1e75749e88cb78fc51adbc83c9090
Sampsa Sohlman <sampsa@sohlman.com>
2026-04-18 15:07:20 +0300

    feat: replace MD5 fingerprint with BLAKE3

--- service/src/main/resources/META-INF/sql/tables.sql
+++ service/src/main/resources/META-INF/sql/tables.sql
@@ -2,34 +2,16 @@ create table bfdms_FileData (
 	fileDataId LONG not null primary key,
 	companyId LONG,
 	createDate DATE null,
-	name VARCHAR(75) null,
+	name VARCHAR(255) null,
 	size_ LONG,
-	fingerprint VARCHAR(75) null
+	fingerprint VARCHAR(255) null
 );
 
 create table bfdms_FileInfo (
 	fileInfoId LONG not null primary key,
 	companyId LONG,
 	repositoryId LONG,
-	path_ VARCHAR(75) null,
-	version VARCHAR(75) null,
-	fileDataId LONG
-);
-
-create table bffss_FileData (
-	fileDataId LONG not null primary key,
-	companyId LONG,
-	createDate DATE null,
-	name VARCHAR(75) null,
-	size_ LONG,
-	fingerprint VARCHAR(75) null
-);
-
-create table bffss_FileInfo (
-	fileInfoId LONG not null primary key,
-	companyId LONG,
-	repositoryId LONG,
-	path_ VARCHAR(75) null,
-	version VARCHAR(75) null,
+	path_ VARCHAR(255) null,
+	version VARCHAR(255) null,
 	fileDataId LONG
 );
